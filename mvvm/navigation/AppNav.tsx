dff92458ce94202e350268de3f952d06058056b8
omerbenarieh <omerrbenarieh@gmail.com>
2024-06-23 13:49:24 +0300

    fix

---
+++ mvvm/navigation/AppNav.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomePage from '../pages/homePage'
+import LoginView from '../pages/loginPage'
+import RegisterView from '../pages/registerPage'
+const Stack = createNativeStackNavigator()
+
+const AppNav = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Home"
+          component={HomePage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginView}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterView}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+export default AppNav
